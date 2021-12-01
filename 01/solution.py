574bf0a2e2b223fe452ecc4e8bdc0442ea9c471f
Alex Lach <alexander_lach@oxy.com>
2021-12-01 16:37:39 -0600

    add oneliner solutions for day 1

--- 01/solution.py
+++ 01/solution.py
@@ -14,7 +14,16 @@ print(count)
 diffs = [dep - dep_os for dep, dep_os in zip(depths[:-1], depths[1:])]
 print(sum(1 for i in diffs if i < 0))
 
+# part 1, oneliner, noting that booleans can be parsed as ints
+print(sum(x < y for x, y in zip(depths, depths[1:])))
+
 # part 2, consider a sliding window of three measurements
 mov_sum = [d1 + d2 + d3 for d1, d2, d3 in zip(depths[2:], depths[1:-1], depths[:-2])]
 diffs = [dep - dep_os for dep, dep_os in zip(mov_sum[:-1], mov_sum[1:])]
 print(sum(1 for i in diffs if i < 0))
+
+# part 2, oneliner, noting that with sliding windows we can ignore the overlap:
+# abc   <- window 1
+#  bcd  <- window 2
+# the only difference between these windows is a and d, since b and c are added to both
+print(sum(x < y for x, y in zip(depths, depths[3:])))
