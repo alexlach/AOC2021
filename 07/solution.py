ca2e26a0a16dc0782217b710b45c78fa5284a95b
Alex Lach <alexander_lach@oxy.com>
2021-12-07 11:32:55 -0600

    smarter day seven

--- 07/solution.py
+++ 07/solution.py
@@ -1,22 +1,19 @@
 positions = open("07/input.txt").read().split(",")
-positions = [int(f) for f in positions]
+positions = sorted([int(f) for f in positions])
 
+center = positions[len(positions) // 2]  # median minimizes sum(abs deviations)
+distances = [abs(center - pos) for pos in positions]
+print(sum(distances))  # part 1
 
-# part 1
-fuel_dict = {}
-for center in range(min(positions), max(positions)):
-    distances = [abs(center - pos) for pos in positions]
-    fuel_dict[center] = sum(distances)
-print(fuel_dict[min(fuel_dict, key=fuel_dict.get)])
+tri_num = lambda n: abs(n) * (abs(n) + 1) // 2
+mean = sum(positions) // len(positions)  # mean is provably within 0.5 of optimum
+fuel_1 = sum([tri_num(mean - pos) for pos in positions])
+fuel_2 = sum([tri_num(mean + 1 - pos) for pos in positions])
+print(min(fuel_1, fuel_2))  # part 2
 
-
-def triangular_number(n):
-    return n * (n + 1) // 2
-
-
-# part 2
-fuel_dict = {}
-for center in range(min(positions), max(positions)):
-    distances = [triangular_number(abs(center - pos)) for pos in positions]
-    fuel_dict[center] = sum(distances)
-print(fuel_dict[min(fuel_dict, key=fuel_dict.get)])
+# part 2, slow way
+# fuel_dict = {}
+# for center in range(min(positions), max(positions)):
+#     distances = [tri_num(abs(center - pos)) for pos in positions]
+#     fuel_dict[center] = sum(distances)
+# print(fuel_dict[min(fuel_dict, key=fuel_dict.get)])
